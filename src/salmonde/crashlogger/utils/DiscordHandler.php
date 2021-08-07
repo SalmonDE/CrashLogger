82a364ac21d9fcbac22553bff41a2ef060449131
SalmonDE <salmonde@salmonde.de>
2021-08-07 14:26:05 +0200

    lowercase namespace

---
+++ src/salmonde/crashlogger/utils/DiscordHandler.php
@@ -0,0 +1,131 @@
+<?php
+declare(strict_types = 1);
+
+namespace salmonde\crashlogger\utils;
+
+use pocketmine\CrashDump;
+use pocketmine\Server;
+use pocketmine\utils\Internet;
+
+class DiscordHandler {
+
+	private const COLOURS = [
+		16761035,
+		346726,
+		15680081,
+		6277471,
+		16439902
+	];
+
+	private $webhookUrl;
+	private $crashDumpReader;
+
+	private $announceCrash = true;
+	private $fullPath = false;
+
+	public function __construct(string $webhookUrl, CrashDumpReader $crashDumpReader, bool $announceCrash = true, bool $fullPath = false){
+		$this->webhookUrl = $webhookUrl;
+		$this->crashDumpReader = $crashDumpReader;
+
+		$this->announceCrash = $announceCrash;
+		$this->fullPath = $fullPath;
+	}
+
+	public function submit(): void{
+		if(!$this->crashDumpReader->hasRead()){
+			return;
+		}
+
+		$serverFolder = $this->fullPath ? Server::getInstance()->getDataPath() : basename(Server::getInstance()->getDataPath());
+		if($this->announceCrash){
+			$this->announceCrash($serverFolder);
+		}
+
+		$webhookData = [];
+		$webhookData["content"] = "Server \"".$serverFolder."\" crashed 👺";
+
+		$crashData = $this->crashDumpReader->getData();
+
+		$infoString = $this->getInfoString($crashData);
+		$codeString = $this->getCodeString($crashData);
+		$traceString = $this->getTraceString($crashData);
+
+		$webhookData["embeds"][] = [
+			"color" => self::COLOURS[array_rand(self::COLOURS)],
+			"title" => substr($crashData["error"]["message"] ?? "Unknown error", 0, 256),
+			"fields" => [
+				[
+					"name" => "Info",
+					"value" => substr($infoString, 0, 1024),
+					"inline" => true
+				],
+				[
+					"name" => "Code",
+					"value" => substr($codeString, 0, 1024),
+					"inline" => true
+				],
+				[
+					"name" => "Trace",
+					"value" => substr($traceString, 0, 1024),
+					"inline" => true
+				]
+			]
+		];
+
+		Internet::postURL($this->webhookUrl, $webhookData, 10, ["Content-Type" => "application/json"]);
+	}
+
+	protected function getInfoString(array $crashData): string{
+		$infoString  = "File: **".$crashData["error"]["file"]."**";
+		$infoString .= "\nLine: **".$crashData["error"]["line"]."**";
+		$infoString .= "\nType: ".$crashData["error"]["type"];
+		$infoString .= "\nTime: ".date("d.m.Y (l): H:i:s [e]");
+		$infoString .= "\nPlugin involved: ".$crashData["plugin_involvement"];
+		$infoString .= "\nPlugin: **".($crashData["plugin"] ?? "?")."**";
+		$infoString .= "\nGit commit: __".$crashData["general"]["git"]."__";
+
+		return $infoString;
+	}
+
+	protected function getCodeString(array $crashData): string{
+		$codeString = "```php";
+
+		$faultyLine = $crashData["error"]["line"];
+		foreach($crashData["code"] as $line => $code){
+			$codeLine = ($line === $faultyLine ? ">" : " ")."[".$line."] ".$code;
+			$codeString .= "\n".$codeLine;
+		}
+
+		$stringEnding = "\n```";
+		$codeString = substr($codeString, 0, 1024 - strlen($stringEnding));
+		$codeString .= $stringEnding;
+
+		return $codeString;
+	}
+
+	protected function getTraceString(array $crashData): string{
+		$traceString = "";
+		foreach($crashData["trace"] as $trace){
+			if(!isset($traceString)){
+				$traceString = $trace;
+				continue;
+			}
+
+			$traceString .= "\n".$trace;
+		}
+
+		return $traceString;
+	}
+
+	final private function announceCrash(string $serverFolder): void{
+		try{
+			$webhookData = [
+				"content" => "Crash detected in \"".$serverFolder."\""
+			];
+
+			Internet::postURL($this->webhookUrl, $webhookData, 10, ["Content-Type" => "application/json"]);
+		}catch(\Throwable $e){
+			Server::getInstance()->getPluginManager()->getPlugin("CrashLogger")->getLogger()->error("Error during crash announcement in file ".$e->getFile()." on line ".$e->getLine().": ".$e->getMessage());
+		}
+	}
+}
